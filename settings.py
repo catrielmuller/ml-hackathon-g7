c32f8c5a240fc001d6f4dcd53d39b400121abfd0
maurosr <maurito.s@gmail.com>
2015-04-11 19:03:48 -0300

    DB prod settings.

--- settings.py
+++ settings.py
@@ -1,7 +1,8 @@
+import os
+
 EVE_SETTINGS = {
     'DOMAIN': {},
     'URL_PREFIX': 'api',
-    'MONGO_DBNAME': 'melinder',
     'XML': False,
     'HATEOAS': False,
     'IF_MATCH': False,
@@ -11,3 +12,14 @@ EVE_SETTINGS = {
     'ITEM_METHODS': ['GET', 'PUT', 'DELETE'],
     'PAGINATION_LIMIT': 10000
 }
+
+
+if os.environ.get('PORT'):
+    EVE_SETTINGS['MONGO_HOST'] = 'ds061671.mongolab.com'
+    EVE_SETTINGS['MONGO_PORT'] = 61671
+    EVE_SETTINGS['MONGO_USERNAME'] = 'ml-hackathon-g7'
+    EVE_SETTINGS['MONGO_PASSWORD'] = '.hackathon.'
+    EVE_SETTINGS['MONGO_DBNAME'] = 'heroku_app35780615'
+else:
+    EVE_SETTINGS['MONGO_DBNAME'] = 'melinder'
+
