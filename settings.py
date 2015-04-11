4ccd225ac9ff7876e1a5ccd8170d353a677f2140
Catriel Müller <catrielmuller@gmail.com>
2015-04-11 19:32:47 -0300

    Test Mongo Uri

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 EVE_SETTINGS = {
     'DOMAIN': {},
@@ -16,6 +17,7 @@ EVE_SETTINGS = {
 
 if os.environ.get('PORT'):
     print 'prod'
+    sys.stdout.flush()
     EVE_SETTINGS['MONGO_HOST'] = 'ds061671.mongolab.com'
     EVE_SETTINGS['MONGO_PORT'] = 61671
     EVE_SETTINGS['MONGO_USERNAME'] = 'ml-hackathon-g7'
@@ -25,5 +27,6 @@ if os.environ.get('PORT'):
     EVE_SETTINGS['MONGO_URI'] = 'mongodb://ml-hackathon-g7:.hackathon.@ds061671.mongolab.com:61671/heroku_app35780615'
 else:
     print 'local'
+    sys.stdout.flush()
     EVE_SETTINGS['MONGO_DBNAME'] = 'melinder'
 
