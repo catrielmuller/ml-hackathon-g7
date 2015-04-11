699e9f840291c55eb67b18f087ba26e397c89faf
Catriel Müller <catrielmuller@gmail.com>
2015-04-11 20:27:34 -0300

    Fix port Heroku

--- run_app.py
+++ run_app.py
@@ -2,9 +2,7 @@ import os
 
 from melinder import app
 
-port = int(os.environ.get('PORT'))
-if not port:
-    port = 5000
-
 app.secret_key = "\xe6\xd7\xcd2\x16\xb8\xa0,\x10\xb8V\xf8\xed\xa01\x9a\xbe\xfb\xa5\x88\xff\x0e\xd5"
-app.run(debug=True)
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
+
