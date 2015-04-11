fcccdad8086b0c8dc0090d9afbbf2a29d1c95ac1
maurosr <maurito.s@gmail.com>
2015-04-11 20:35:55 -0300

    Product collection.

---
+++ components/products/schema.py
@@ -0,0 +1,5 @@
+product_schema = {
+    'product_id': {'type': 'string', 'required': True},
+    'image_url': {'type': 'string'},
+    'description': {'type': 'string'}
+}
--- run_app.py
+++ run_app.py
@@ -2,8 +2,10 @@ import os
 
 from melinder import app
 
-port = int(os.environ.get('PORT'))
-if not port:
+port = os.environ.get('PORT')
+if port:
+    port = int(port)
+else:
     port = 5000
 
 app.secret_key = "\xe6\xd7\xcd2\x16\xb8\xa0,\x10\xb8V\xf8\xed\xa01\x9a\xbe\xfb\xa5\x88\xff\x0e\xd5"
