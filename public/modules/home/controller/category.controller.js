21340732a0a8bd2bb95b1841017b3aac8fe6c8eb
Ignacio <ignacio@venzee.com>
2015-04-12 07:57:44 -0300

    debug

--- public/modules/home/controller/category.controller.js
+++ public/modules/home/controller/category.controller.js
@@ -13,6 +13,7 @@ angular.module('melinder')
 
         $scope.guardarCategories=function(categories){
             var catPost = [];
+            console.log( $scope.categories.length ? true : false);
             for(var i = 0; i < $scope.categories.length; i++ ){
                 if($scope.categories[i].checked){
                     catPost.push($scope.categories[i].id);
