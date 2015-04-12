bcd7a33b7dc1754778fd02b9e89fe48c2a351cf6
Diego <diegolis@gmail.com>
2015-04-12 05:31:19 -0300

    Agregar viewed. Search

--- components/offers/schema.py
+++ components/offers/schema.py
@@ -2,5 +2,6 @@ offer_schema = {
     'seller': {'type': 'objectid', 'data_relation': {'resource': 'user', 'embeddable': True}},
     'item_id': {'type': 'string', 'required': True},
     'product': {'type': 'objectid', 'data_relation': {'resource': 'product', 'embeddable': True}},
-    'price': {'type': 'number', 'required': True}
+    'price': {'type': 'number', 'required': True},
+    'viewed': {'type': 'boolean'}
 }
--- melinder/buyer.py
+++ melinder/buyer.py
@@ -10,10 +10,12 @@ from login import meli, login_required
 @app.route('/api/product/<product_id>/like')
 @login_required
 def like(product_id):
-    current_app.data.insert('like', {'does_like': request.args.get('value'), 'user': session['user_id'], 'product': product_id})
-    product = current_app.data.find("product", parse_request('product'), {'_id': like.product})
-    category = current_app.data.find("category", parse_request('category'), {'_id': product.category})
+    product = current_app.data.find("product", parse_request('product'), {'_id': product_id})[0]
+    category = current_app.data.find("category", parse_request('category'), {'_id': product['category']})[0]
 
-    items = json.loads(meli.get("/sites/MLA/search/?category=%s&q=%s" % (category.meli_id, product.description)).content)
+    items = json.loads(meli.get("/sites/MLA/search/?category=%s&q=%s" % (category['meli_id'], product['description'])).content)
+    #items = json.loads(meli.get("/sites/MLA/search/?q=%s" % category['meli_id']).content)
 
-    return items
+    #current_app.data.insert('like', {'does_like': request.args.get('value'), 'user': session['user_id'], 'product': product_id})
+
+    return json.dumps(items)
