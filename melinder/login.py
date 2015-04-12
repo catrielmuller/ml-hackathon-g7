47a058725a85211348eec0d9f65f917f977d1dcb
maurosr <maurito.s@gmail.com>
2015-04-11 22:32:57 -0300

    Fixed insert.

--- melinder/login.py
+++ melinder/login.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, request, session
+from flask import render_template, redirect, request, session, current_app
 from eve import methods as api
 import json
 import requests
@@ -27,28 +27,23 @@ def login():
 
 @app.route("/authorize")
 def authorize():
-	if request.method == 'GET':
-		meli.authorize(code=request.args.get('code', ''), redirect_URI=REDIRECT_URI)
+    if request.method == 'GET':
+        meli.authorize(code=request.args.get('code', ''), redirect_URI=REDIRECT_URI)
 
-		user = json.loads(meli.get("/users/me", {'access_token': session['access_token']}).content)
-		exists = api.get('user', {'where': '{"meli_id": "%d"}' % user['id']})[0]['_items']
-		if not exists:
-			#return API + "user/?" +  json.dumps({"meli_id": str(user['id']), "email": user['email']})
-			response = requests.post('http://localhost:5000/api/user', json={'meli_id': '1', 'email': 'diegolis@gmail.com'}, headers={'Content-Type': 'application/json'})
-			print response
-			#requests.post(API + "user/", json=json.dumps({"meli_id": str(user['id']), "email": user['email']}), headers={"Content-Type": "application/json"})
-			assert False
-			#api.put('user', {"meli_id": user['id'], "email": user['email']}, headers={"Content-Type": "application/json"})
+    session['access_token'] = meli.access_token
+    session['refresh_token'] = meli.refresh_token
 
+    user = json.loads(meli.get("/users/me", {'access_token': session['access_token']}).content)
+    exists = api.get('user', {'where': '{"meli_id": "%d"}' % user['id']})[0]['_items']
+    if not exists:
+        current_app.data.insert('user', {'meli_id': 6, 'email': 'bla'})
 
-		session['access_token'] = meli.access_token
-		session['refresh_token'] = meli.refresh_token
-		return redirect("/")
+    return redirect("/")
 
 
 @app.route("/")
 def index():
-	if not 'access_token' in session or not 'refresh_token' in session:
-		return redirect("/login")
-	return redirect("/index.html")
+    if not 'access_token' in session or not 'refresh_token' in session:
+        return redirect("/login")
+    return redirect("/index.html")
 
