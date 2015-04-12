fbcd5556ae82a8f519d92c3e7fda8c718368a9e7
Diego <diegolis@gmail.com>
2015-04-12 00:34:11 -0300

    Test para hace searches

--- melinder/views.py
+++ melinder/views.py
@@ -1,5 +1,5 @@
 from melinder import app
-from flask import send_from_directory, session
+from flask import send_from_directory, session, current_app
 import os
 import json
 
@@ -25,3 +25,22 @@ def index():
 def static_proxy(path):
     return send_from_directory(STATIC_FOLDER, path)
 
+
+@login_required
+@app.route('/api/offer/<offer>/change_price/')
+def change_price():
+    # chequear que <offer> sea de <me>
+    # publicar un item igual a <offer>
+    # devolver item
+    #meli.get("/sites/MLA/search/?category=MLA5725&q=ipod", {'access_token': session['access_token']}).content
+    return ""
+
+
+
+@login_required
+@app.route('/test')
+def test():
+    current_app.data.insert('like', {'like': 'true'})
+
+    #meli.get("/sites/MLA/search/?category=MLA5725&q=ipod", {'access_token': session['access_token']}).content
+    return ""
