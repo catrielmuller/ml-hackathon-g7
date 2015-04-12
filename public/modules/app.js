b540c2ef46a3355cdf4131fe0a3db62e92878ce4
Ignacio <ignacio@venzee.com>
2015-04-11 21:01:00 -0300

    fix router

--- public/modules/app.js
+++ public/modules/app.js
@@ -6,20 +6,6 @@ var app = angular.module('melinder', venderLibs);
 
 app.config(function($stateProvider, $urlRouterProvider) {
 
-        $urlRouterProvider.otherwise('/home');
-
-        $stateProvider
-
-            // HOME STATES AND NESTED VIEWS ========================================
-            .state('home', {
-                url: '/home',
-                templateUrl: 'modules/home/view/home.html'
-                //template: '<div>partial-home.html</div>,<div>partial-home.html</div>,<div>partial-home.html</div>,<div>partial-home.html</div>'
-            })
-
-            // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-            .state('about', {
-                // we'll get to this in a bit
-            });
-
+        $urlRouterProvider.otherwise('/');
+    
     });
