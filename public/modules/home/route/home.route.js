486f69a054cc7ab06e9086c1ebfc7a802555a4e2
Ignacio <ignacio@venzee.com>
2015-04-12 01:55:04 -0300

    add intro

--- public/modules/home/route/home.route.js
+++ public/modules/home/route/home.route.js
@@ -8,12 +8,24 @@ angular.module('melinder')
 
             .state('home', {
                 url: '/',
-                templateUrl: 'modules/home/view/home.html',
+                templateUrl: 'modules/home/view/intro.html',
                 resolve:{
                     userLogged:['userService',function(userService){
                         return userService.getUserLoggued();
-                    }]
+                    }],
+                    intro : function($timeout, $state){
+                        $timeout(function(){
+                            console.log('me voooy');
+                            $state.transitionTo('menu');
+                        },3000)
+                    }
+
                 }
+
+            })
+            .state('menu', {
+                url: '/menu',
+                templateUrl: 'modules/home/view/home.html'
 
             })
 
