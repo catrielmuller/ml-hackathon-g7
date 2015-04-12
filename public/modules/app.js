d1dbd349752a1211ef75d0e3ae782974b05266a1
Ignacio <ignacio@venzee.com>
2015-04-11 21:18:08 -0300

    init ionic

--- public/modules/app.js
+++ public/modules/app.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var venderLibs = ['ui.router'];
+var venderLibs = ['ui.router', 'ionic'];
 
 var app = angular.module('melinder', venderLibs);
 
 app.config(function($stateProvider, $urlRouterProvider) {
 
         $urlRouterProvider.otherwise('/');
-    
+
     });
